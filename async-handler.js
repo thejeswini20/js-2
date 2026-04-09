310b9dadedc5e24a64a8df0452de7410a88f7aea
thejeswini20 <thejaprincess390@gmail.com>
2026-04-09 20:07:28 +0530

    Add files via upload

---
+++ async-handler.js
@@ -0,0 +1,9 @@
+import { request } from "express"
+
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise
+    .resolve(requestHandler(req, res, next))
+    .catch((error) => next(error));
+  }
+}
