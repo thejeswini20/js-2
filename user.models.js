e10a8e1c4794354825e59da8c6aafcf4c9935d39
thejeswini20 <thejaprincess390@gmail.com>
2026-04-11 21:17:06 +0530

    Add files via upload

--- user.models.js
+++ user.models.js
@@ -1,5 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 const userSchema = new Schema(
   {
      avatar:{
@@ -61,5 +63,40 @@ userSchema.methods.isPasswordCorrect = async function(password) {
    return await bcrypt.compare(password, this.password);
 };
 
+userSchema.methods.generateAccessToken = function() {
+   jwt.sign(
+    {
+       _id: this._id,
+       username: this.username,
+       email: this.email
+    },
+     process.env.ACCESS_TOKEN_SECRET,
+     {expiresIn: process.env.ACCESS_TOKEN_EXPIRY  }
+   )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+  return jwt.sign(
+    {
+       _id: this._id,
+    },
+     process.env.REFRESH_TOKEN_SECRET,
+     {expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+);
+};
+
+userSchema.methods.generateTemporaryToken = function() {
+     crypto.randomBytes(20).toString("hex")
+
+     const hashedToken = crypto
+     .createHash("sha256")
+     .update(unHashedToken)
+     .digest("hex");
+
+   const tokenExpiry = Date.now() + (20 * 60 * 1000);
+
+   return {hashedToken, tokenExpiry, unHashedToken}
+}
 export const User = mongoose.model("User", userSchema)
 
